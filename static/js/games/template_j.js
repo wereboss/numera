308fc494ff6abb39be3adfd5883e666e93e56af5
Srinath Menon <tellsrinath@gmail.com>
2026-06-23 11:18:41 +0800

    Increase post-round delay to 4 seconds to leave a 2 second gap after audio completes

--- static/js/games/template_j.js
+++ static/js/games/template_j.js
@@ -237,7 +237,7 @@ const TemplateJ = {
                 } else {
                     this.triggerFinale(document.getElementById('game-canvas'));
                 }
-            }, 2000);
+            }, 4000);
         } else {
             this.state.locked = true;
             cardEl.classList.add('incorrect-choice');
